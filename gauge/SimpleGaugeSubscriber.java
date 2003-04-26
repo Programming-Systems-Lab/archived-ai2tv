43c5bc0860745c047f513df9e37edc1d3c92ab26
mp2079 <mp2079>
2003-04-26 23:10:20 +0000

    update to optionally connect using ThinClient (if a system property is set) but not sure if that actually works or not

--- gauge/SimpleGaugeSubscriber.java
+++ gauge/SimpleGaugeSubscriber.java
@@ -6,21 +6,34 @@ import siena.comm.*;
 import java.util.Hashtable;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 
 /**
 	Basic Siena-based event receiver for the gauge.
 	Abstract since the implementation of @see notify() is left to subclasses
 */
 public abstract class SimpleGaugeSubscriber implements GaugeSubscriber {
-	private Filter gaugeFilter;
-	private HierarchicalDispatcher mainSiena;
+
+    private static final Logger logger = Logger.getLogger(SimpleGaugeSubscriber.class);
+
+    private Filter gaugeFilter;
+	private Siena mainSiena;
 	
 		/** Siena and subscriptions initialization */
 	protected void setup() 
 		throws SienaException, IOException {
-	    mainSiena = new HierarchicalDispatcher();
-	    mainSiena.setReceiver(new UDPPacketReceiver(sienaPort));
-	    System.out.println ("Siena Server Up: " + new String(mainSiena.getReceiver().address()));
+            String server = System.getProperty("ai2tv.server");
+            if (server != null) {
+
+                mainSiena = new ThinClient(server);
+                logger.debug("Connnected to server at " + ((ThinClient)mainSiena).getServer());
+            }
+            else {
+                mainSiena = new HierarchicalDispatcher();
+                ((HierarchicalDispatcher) mainSiena).setReceiver(new UDPPacketReceiver(sienaPort));
+	            logger.debug ("Siena Server Up: " + new String(((HierarchicalDispatcher) mainSiena).getReceiver().address()));
+            }
 
 	    Filter startFilter = new Filter();
 	    startFilter.addConstraint("Start", Op.ANY, (String)null);
