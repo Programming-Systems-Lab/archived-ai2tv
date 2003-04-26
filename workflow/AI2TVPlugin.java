99245713c09406591e7e0b7e5c79a661c0ad39d8
mp2079 <mp2079>
2003-04-26 21:32:51 +0000

    initial commit. This plugin receives ReportAssets from GaugeLDMPluginIn and publishes an ai2tv LittleJIL diagram and associated resources

---
+++ workflow/AI2TVPlugin.java
@@ -0,0 +1,177 @@
+package psl.ai2tv.workflow;
+
+import psl.ai2tv.workflow.assets.ClientAsset;
+import psl.ai2tv.workflow.assets.PropertyGroupFactory;
+import psl.ai2tv.workflow.assets.ReportAsset;
+import psl.workflakes.littlejil.ExecutableTask;
+import psl.workflakes.littlejil.LittleJILResourceTable;
+import psl.workflakes.littlejil.TaskExpanderPlugin;
+
+import org.cougaar.core.blackboard.IncrementalSubscription;
+import org.cougaar.core.domain.RootFactory;
+import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.service.DomainService;
+import org.cougaar.core.service.PrototypeRegistryService;
+import org.cougaar.util.UnaryPredicate;
+
+import laser.littlejil.Diagram;
+import laser.littlejil.Program;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * This plugin subscribes to ReportAssets published by the GaugeLDMPlugin, and for each report it
+ * "registers" the resources appropriately in the resources table and publishes a new Ai2TV LittleJIL diagram.
+ * @author matias
+ */
+public class AI2TVPlugin extends ComponentPlugin {
+
+    private static final Logger logger = Logger.getLogger(TaskExpanderPlugin.class);
+    private IncrementalSubscription resourceTableSubscription;
+    private IncrementalSubscription reportAssetSubscription;
+
+    private RootFactory factory;
+
+    byte[] diagramByteArray;    // cache of the serialized LittleJIL ai2tv diagram
+
+    private static class ResourceTablePredicate implements UnaryPredicate {
+        public boolean execute(Object o) {
+            return (o instanceof LittleJILResourceTable);
+        }
+    }
+
+    private static class ReportAssetPredicate implements UnaryPredicate {
+        public boolean execute(Object o) {
+            return (o instanceof ReportAssetPredicate);
+        }
+    }
+
+    public AI2TVPlugin() throws IOException {
+
+        // read in the serialized ai2tv diagram... we will instantiate a new one each time we run the workflow
+        // we keep the serialized byte array in memory so as not to have to read from the file every time
+        String filename = System.getProperty("ai2tv.diagram");
+        File file = new File(filename);
+        if (!file.exists()) {
+            throw new FileNotFoundException(filename);
+        }
+
+        diagramByteArray = new byte[(int)file.length()];
+
+        BufferedInputStream bufIn =  new BufferedInputStream(new FileInputStream(file));
+        bufIn.read(diagramByteArray);
+        bufIn.close();
+
+    }
+
+    /**
+     * Used by the binding utility through reflection to set my DomainService
+     */
+    public void setDomainService(DomainService domainService) {
+        factory = domainService.getFactory();
+    }
+
+    public void setPrototypeRegistryService(PrototypeRegistryService prototypeRegistryService) {
+
+        factory.addPropertyGroupFactory(new PropertyGroupFactory());
+
+        // set the Prototypes for ClientAssets
+        ClientAsset clientProto = (ClientAsset)factory.createPrototype(ClientAsset.class, "ClientProto");
+        prototypeRegistryService.cachePrototype("ClientProto", clientProto);
+
+    }
+
+    public void setupSubscriptions() {
+
+        resourceTableSubscription = (IncrementalSubscription) blackboard.subscribe(new ResourceTablePredicate());
+        reportAssetSubscription = (IncrementalSubscription) blackboard.subscribe(new ReportAssetPredicate());
+
+    }
+
+    public void execute() {
+
+        LittleJILResourceTable resourceTable;
+        if (resourceTableSubscription.size() == 0) {
+            // create a new resource table
+            resourceTable = new LittleJILResourceTable();
+            blackboard.publishAdd(resourceTable);
+        } else {
+            resourceTable = (LittleJILResourceTable) resourceTableSubscription.first();
+        }
+
+        for (Enumeration reports = reportAssetSubscription.getAddedList(); reports.hasMoreElements();) {
+
+            ReportAsset reportAsset = (ReportAsset) reports.nextElement();
+
+            // instantiate a new ai2tv LittleJIL diagram
+            ObjectInputStream objIn = null;
+            try {
+                objIn = new ObjectInputStream(new ByteArrayInputStream(diagramByteArray));
+
+                Program program = (Program) objIn.readObject();
+                Diagram diagram = program.getRootDiagram();
+
+                // add resources for this diagram
+
+                resourceTable.addResource(diagram, "clients", reportAsset.getBucketPG().getGroup());
+
+                // for base client we just instantiate a new ClientAsset
+                ClientAsset baseClient = (ClientAsset) factory.createInstance("ClientProto");
+                baseClient.setClientPG(factory.createPropertyGroup("ClientPG"));
+                baseClient.setFramePG(factory.createPropertyGroup("FramePG"));
+
+                resourceTable.addResource(diagram, "baseClient", baseClient);
+
+            } catch (Exception e) {
+                logger.warn("Could not instantiate new diagram: " + e);
+            }
+
+        }
+
+    }
+
+    public static class DummyExecutableTask implements ExecutableTask {
+
+        public void execute(String method, Hashtable inParams, Hashtable outParams) throws Exception {
+
+            logger.info("executing method " + method);
+
+            if (method.equals("FindBase")) {
+
+                Vector clients = (Vector) inParams.get("clients");
+                ClientAsset baseClient = (ClientAsset) inParams.get("baseClient");
+
+                logger.debug("FindBase got clients " + clients + ", base=" + baseClient);
+
+            }
+            else if (method.equals("EvaluateClient")) {
+
+                ClientAsset baseClient = (ClientAsset) inParams.get("baseClient");
+                ClientAsset client = (ClientAsset) inParams.get("clients");
+
+                logger.debug("EvaluateClient got client=" + client + ", base=" + baseClient);
+
+            }
+            else if (method.equals("AdaptClient")) {
+
+                ClientAsset client = (ClientAsset) inParams.get("clients");
+                logger.debug("AdaptClients got client: " + client);
+
+            }
+            else {
+
+                logger.debug("unknown method " + method);
+            }
+
+        }
+
+    }
+}
+
+
+
+
