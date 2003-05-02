bbbcddeefc3be373adab15358541ef1d5c1067d4
dp2041 <dp2041>
2003-05-02 21:27:51 +0000

    added some more constants

--- SienaConstants.java
+++ SienaConstants.java
@@ -5,21 +5,32 @@ package psl.ai2tv;
  *
  */
 public class SienaConstants {
-    public static final String AI2TV_FRAME = "AI2TV_FRAME";
-    public static final String CLIENT_ID = "CLIENT_ID";
-    public static final String BANDWIDTH = "bandwidth";
-    public static final String PROBE_TIME = "probeTime";
-    public static final String LEFTBOUND = "leftbound";
-    public static final String RIGHTBOUND = "rightbound";
-    public static final String MOMENT = "moment";
-    public static final String LEVEL = "level";
-    public static final String SIZE = "size";
+  public static final String AI2TV_FRAME = "AI2TV_FRAME";
+  public static final String CLIENT_ID = "CLIENT_ID";
+  public static final String BANDWIDTH = "bandwidth";
+  public static final String PROBE_TIME = "probeTime";
+  public static final String LEFTBOUND = "leftbound";
+  public static final String RIGHTBOUND = "rightbound";
+  public static final String MOMENT = "moment";
+  public static final String LEVEL = "level";
+  public static final String SIZE = "size";
+  public static final String TIME_SHOWN = "timeShown";
+  
+  public static final String AI2TV_VIDEO_ACTION = "AI2TV_VIDEO_ACTION";
+  public static final String CHANGE_LEVEL = "CHANGE_LEVEL";
+  public static final String CHANGE_LEVEL_UP = "UP";
+  public static final String CHANGE_LEVEL_DOWN = "DOWN";
+  public static final String PLAY = "PLAY";
+  public static final String STOP = "STOP";
+  public static final String PAUSE = "PAUSE";
+  public static final String GOTO = "GOTO";
+  public static final String NEWTIME = "NEWTIME";
 
-    public static final String AI2TV_VIDEO_ACTION = "AI2TV_VIDEO_ACTION";
-    public static final String UP_LEVEL = "UP_LEVEL";
-    public static final String DOWN_LEVEL = "DOWN_LEVEL";
-    public static final String PLAY = "PLAY";
-    public static final String STOP = "STOP";
-    public static final String PAUSE = "PAUSE";
-    public static final String GOTO = "GOTO";
+  public static final String AI2TV_FRAME_UPDATE = "AI2TV_FRAME_UPDATE";
+  public static final String PLAN_FOR = "PLAN_FOR";
+
+  public static final String AI2TV_WF_ECHO = "AI2TV_WF_ECHO";
+  public static final String AI2TV_WF_ECHO_REPLY = "AI2TV_WF_ECHO_REPLY";
+  public static final String AI2TV_WF_ECHO_SENT_TIME = "SENT_TIME";
+  public static final String AI2TV_WF_ECHO_RCVD_TIME = "RCVD_TIME";
 }
