966669152f115ba9520eef971ee84567d4f498e0
dp2041 <dp2041>
2003-05-08 21:52:25 +0000

    added comments and changed names of some constants to be more intuitive

--- SienaConstants.java
+++ SienaConstants.java
@@ -5,6 +5,8 @@ package psl.ai2tv;
  *
  */
 public class SienaConstants {
+
+  // these represent the typical client probe frame updates
   public static final String AI2TV_FRAME = "AI2TV_FRAME";
   public static final String CLIENT_ID = "CLIENT_ID";
   public static final String BANDWIDTH = "bandwidth";
@@ -17,23 +19,27 @@ public class SienaConstants {
   public static final String SIZE = "size";
   public static final String TIME_SHOWN = "timeShown";
   
+  // these represent the actions of the clients, and are typically sent
+  // between peers
   public static final String AI2TV_VIDEO_ACTION = "AI2TV_VIDEO_ACTION";
-  public static final String CHANGE_LEVEL = "CHANGE_LEVEL";
-  public static final String CHANGE_LEVEL_UP = "UP";
-  public static final String CHANGE_LEVEL_DOWN = "DOWN";
   public static final String PLAY = "PLAY";
   public static final String STOP = "STOP";
   public static final String PAUSE = "PAUSE";
   public static final String GOTO = "GOTO";
   public static final String NEWTIME = "NEWTIME";
 
-  public static final String AI2TV_FRAME_UPDATE = "AI2TV_FRAME_UPDATE";
+  // these are from the WF to adjust certain clients
+  public static final String AI2TV_CLIENT_ADJUST = "AI2TV_CLIENT_ADJUST";
+  public static final String CHANGE_LEVEL = "CHANGE_LEVEL";
+  public static final String CHANGE_LEVEL_UP = "UP";
+  public static final String CHANGE_LEVEL_DOWN = "DOWN";
   public static final String PLAN_FOR = "PLAN_FOR";
 
-  public static final String AI2TV_WF_ECHO = "AI2TV_WF_ECHO";
-  public static final String AI2TV_WF_ECHO_REPLY = "AI2TV_WF_ECHO_REPLY";
-  public static final String AI2TV_WF_ECHO_SENT_TIME = "SENT_TIME";
-  public static final String AI2TV_WF_ECHO_RCVD_TIME = "RCVD_TIME";
+  // these are for the WF to gather certain timing stats
+  public static final String AI2TV_WF_UPDATE_REQUEST = "AI2TV_WF_UPDATE_REQUEST";
+  public static final String AI2TV_WF_UPDATE_REPLY = "AI2TV_WF_UPDATE_REPLY";
+  public static final String AI2TV_UPDATE_TIME_SENT = "TIME_SENT";
+  public static final String AI2TV_UPDATE_TIME_RCVD = "TIME_RECEIVED";
 
   public static final String AI2TV_VIDEO_PREFETCH = "";
 
