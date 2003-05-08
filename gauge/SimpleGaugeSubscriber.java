c9450875b2f635de3397623f45b9630814a23488
dp2041 <dp2041>
2003-05-08 05:24:22 +0000

    added filters for video actions

--- gauge/SimpleGaugeSubscriber.java
+++ gauge/SimpleGaugeSubscriber.java
@@ -11,47 +11,57 @@ import psl.ai2tv.SienaConstants;
 
 
 /**
-	Basic Siena-based event receiver for the gauge.
-	Abstract since the implementation of @see notify() is left to subclasses
+   Basic Siena-based event receiver for the gauge.
+   Abstract since the implementation of @see notify() is left to subclasses
 */
 public abstract class SimpleGaugeSubscriber implements GaugeSubscriber {
 
-    private static final Logger logger = Logger.getLogger(SimpleGaugeSubscriber.class);
+  private static final Logger logger = Logger.getLogger(SimpleGaugeSubscriber.class);
 
-    private Filter gaugeFilter;
-	private Siena mainSiena;
+  private Filter gaugeFilter;
+  private Siena mainSiena;
 	
-		/** Siena and subscriptions initialization */
-	protected void setup() 
-		throws SienaException, IOException {
-            String server = System.getProperty("ai2tv.server");
-            if (server != null) {
+  /** Siena and subscriptions initialization */
+  protected void setup() 
+    throws SienaException, IOException {
+    String server = System.getProperty("ai2tv.server");
+    if (server != null) {
 
-                mainSiena = new ThinClient(server);
+      mainSiena = new ThinClient(server);
 
-                //logger.debug("Connnected to server at " + ((ThinClient)mainSiena).getServer());
-            }
-            else {
-                mainSiena = new HierarchicalDispatcher();
-                ((HierarchicalDispatcher) mainSiena).setReceiver(new KAPacketReceiver(sienaPort));
-	            logger.debug ("Siena Server Up: " + new String(((HierarchicalDispatcher) mainSiena).getReceiver().address()));
-            }
-
-	    Filter startFilter = new Filter();
-	    startFilter.addConstraint(SienaConstants.PLAY, Op.ANY, (String)null);
-	    mainSiena.subscribe(startFilter, this);
-	    
-	    gaugeFilter = new Filter();
-	    gaugeFilter.addConstraint(SienaConstants.AI2TV_FRAME, Op.ANY);
-	    mainSiena.subscribe(gaugeFilter, this);
-
-        Filter filter = new Filter();
-        filter.addConstraint(SienaConstants.AI2TV_FRAME, "");
-        mainSiena.subscribe(filter, this);
-	}
-
-    public void notify(Notification s[]) {
-    	// I never subscribe for patterns anyway. 
+      //logger.debug("Connnected to server at " + ((ThinClient)mainSiena).getServer());
     }
+    else {
+      mainSiena = new HierarchicalDispatcher();
+      ((HierarchicalDispatcher) mainSiena).setReceiver(new KAPacketReceiver(sienaPort));
+      logger.debug ("Siena Server Up: " + new String(((HierarchicalDispatcher) mainSiena).getReceiver().address()));
+    }
+
+    // should ask peppo how do to this more efficiently
+    Filter filter = new Filter();
+    filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, SienaConstants.PLAY);
+    mainSiena.subscribe(filter, this);
+    
+    filter = new Filter();
+    filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, SienaConstants.STOP);
+    mainSiena.subscribe(filter, this);
+
+    filter = new Filter();
+    filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, SienaConstants.PAUSE);
+    mainSiena.subscribe(filter, this);
+
+    filter = new Filter();
+    filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, SienaConstants.GOTO);
+    mainSiena.subscribe(filter, this);
+
+
+    gaugeFilter = new Filter();
+    gaugeFilter.addConstraint(SienaConstants.AI2TV_FRAME, Op.ANY);
+    mainSiena.subscribe(gaugeFilter, this);
+  }
+
+  public void notify(Notification s[]) {
+    // I never subscribe for patterns anyway. 
+  }
 
 }
