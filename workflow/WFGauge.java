e85b9ef31f10bcf1f1af0be9f18b7d79710fdb5b
dp2041 <dp2041>
2003-05-08 05:26:52 +0000

    added internal clock to WFGauge, and started listening and responding to video player events

--- workflow/WFGauge.java
+++ workflow/WFGauge.java
@@ -1,108 +1,144 @@
 package psl.ai2tv.workflow;
 
 import psl.ai2tv.gauge.*;
-
 import java.util.Iterator;
-
 import org.apache.log4j.Logger;
+import psl.ai2tv.client.TimeController;
 
 class WFGauge extends GroupGauge {
 
-    private static final Logger logger = Logger.getLogger(WFGauge.class);
+  private static final Logger logger = Logger.getLogger(WFGauge.class);
+  private GaugeLDMPlugIn LDMHandle = null;
+  private TimeController _clock;
 
-    private GaugeLDMPlugIn LDMHandle = null;
+  private long _lastCheckTime;
 
-    public void setLDMHandle(GaugeLDMPlugIn pi) {
-        LDMHandle = pi;
+  WFGauge(){
+    _clock = new TimeController();
+    _lastCheckTime = -1;
+  }
+
+
+  public void setLDMHandle(GaugeLDMPlugIn pi) {
+    LDMHandle = pi;
+  }
+
+  // overrides of GroupGauge
+
+  protected GaugeComm setupCommunications()
+    throws CommunicationException {
+    GaugeSubscriber subscriber;
+    try {
+      subscriber = new WFSubscriber(this);
+    } catch (Exception e) {
+      subscriber = null;
+      throw new CommunicationException(e);
+    }
+    return subscriber;
+  }
+
+  protected Thread defineNominalClient() {
+    return new Thread() {
+	boolean cont = true;
+
+	public void run() {
+	  running = true;
+	  while (cont) {
+	    try {
+	      // gauge sampling interval
+	      sleep((int) nomInterval);
+	    } catch (InterruptedException ie) {
+	    }
+	    //nomProgress is the time elapsed since client start time
+	    //dp2041 changed, debug
+	    // progress = (int) (System.currentTimeMillis() - startTime);
+	    progress = (int) currentTime();
+	    // in the time elapsed (in secs.) 30 frames per second have been nominally shown
+	    nomProgress = (int) (30 * progress / GroupGauge.SAMPLE_INTERVAL);
+	    //logger.debug("nominalClientThread calling evaluate progress");
+	    try {
+	      evaluateStatus(progress);
+	    }
+	    catch (Exception e) {
+	      logger.error("Exception in evaluateStatus: " + e);
+	    }
+	  }
+	  running = false;
+	}
+      };
+  }
+
+  protected void fillBucket(long elapsed) {
+    Iterator allClients = groupClients.keySet().iterator();
+    while (allClients.hasNext()) {
+      String id = (String) allClients.next();
+      ClientDesc cd = (ClientDesc) groupClients.get(id);
+      FrameDesc fd = cd.getFrame();
+      // long t = fd.getDownloadedTime();
+      long t = currentTime(); // gets the time from our internal clock
+
+      // update the bucket if the time of the last info about a client
+      // is within the time of the last sample and this moment
+      // if (t <= elapsed && t > bucket.getTime()) {
+      
+      if (t > _lastCheckTime) {
+	_lastCheckTime = t;
+	logger.debug("updating bucket for client " + id);
+	bucket.update(id, cd);
+      }
+      else {
+	logger.debug("NOT updating bucket for client " + id);
+	logger.debug("t=" + t + ", elapsed=" + elapsed + ", bucket.getTime()=" + bucket.getTime());
+      }
     }
 
-    // overrides of GroupGauge
-
-    protected GaugeComm setupCommunications()
-            throws CommunicationException {
-        GaugeSubscriber subscriber;
-        try {
-            subscriber = new WFSubscriber(this);
-        } catch (Exception e) {
-            subscriber = null;
-            throw new CommunicationException(e);
-        }
-        return subscriber;
-    }
-
-    protected Thread defineNominalClient() {
-        return new Thread() {
-            boolean cont = true;
-
-            public void run() {
-                running = true;
-                while (cont) {
-                    try {
-                        // gauge sampling interval
-                        sleep((int) nomInterval);
-                    } catch (InterruptedException ie) {
-                    }
-                    //nomProgress is the time elapsed since client start time
-                    progress = (int) (System.currentTimeMillis() - startTime);
-                    // in the time elapsed (in secs.) 30 frames per second have been nominally shown
-                    nomProgress = (int) (30 * progress / GroupGauge.SAMPLE_INTERVAL);
-                    //logger.debug("nominalClientThread calling evaluate progress");
-                    try {
-                        evaluateStatus(progress);
-                    }
-                    catch (Exception e) {
-                        logger.error("Exception in evaluateStatus: " + e);
-                    }
-                }
-                running = false;
-            }
-        };
-    }
-
-    protected void fillBucket(long elapsed) {
-        Iterator allClients = groupClients.keySet().iterator();
-        while (allClients.hasNext()) {
-            String id = (String) allClients.next();
-            ClientDesc cd = (ClientDesc) groupClients.get(id);
-            FrameDesc fd = cd.getFrame();
-            long t = fd.getDownloadedTime();
-
-            // update the bucket is the time of the last info about a client
-            // is within the time of the last sample and this moment
-            if (t <= elapsed && t > bucket.getTime()) {
-                logger.debug("updating bucket for client " + id);
-                bucket.update(id, cd);
-            }
-            else {
-                logger.debug("NOT updating bucket for client " + id);
-                logger.debug("t=" + t + ", elapsed=" + elapsed + ", bucket.getTime()=" + bucket.getTime());
-            }
-        }
-
-        bucket.setTime(elapsed);
-    }
+    bucket.setTime(elapsed);
+  }
 
 
-    protected void evaluateStatus(long elapsed) {
-        fillBucket(elapsed);
-        publishStatus();
-        //clearStatus();
-    }
+  protected void evaluateStatus(long elapsed) {
+    fillBucket(elapsed);
+    publishStatus();
+    //clearStatus();
+  }
 
 
-    protected void publishStatus() {
-        //pass it to LDM plugin for publication
-        //System.out.println (bucket);
-        LDMHandle.setReport(bucket);
-    }
+  protected void publishStatus() {
+    //pass it to LDM plugin for publication
+    //System.out.println (bucket);
+    LDMHandle.setReport(bucket);
+  }
 
-    protected FrameIndexParser setFrameInfo() {
-        FrameIndexParser ret = super.setFrameInfo();
+  protected FrameIndexParser setFrameInfo() {
+    FrameIndexParser ret = super.setFrameInfo();
 
-        //now compute equivalent frames for each frame
-        EquivClasses ec = new EquivClasses(ret);
-        ec.computeAllEquivalents(0);
-        return ret;
-    }
+    //now compute equivalent frames for each frame
+    EquivClasses ec = new EquivClasses(ret);
+    ec.computeAllEquivalents(0);
+    return ret;
+  }
+
+  // time controller interfacing functions.  Assuming that the WFGauge
+  // is a focal point in the WF.  these functions could easily be
+  // moved to another class.
+  protected void startTime(){
+    _clock.startTime();
+  }
+
+  protected void pauseTime(){
+    _clock.pause();    
+  }
+
+  protected void stopTime(){
+    _clock.stopTime();        
+  }
+
+  protected void gotoTime(int newtime){
+    _clock.gotoTime(newtime);
+  }
+
+  protected long currentTime(){
+    return _clock.currentTime();
+  }
 
 }
--- workflow/WFSubscriber.java
+++ workflow/WFSubscriber.java
@@ -14,58 +14,69 @@ import org.apache.log4j.Logger;
 
 class WFSubscriber extends SimpleGaugeSubscriber {
 
-    private static final Logger logger = Logger.getLogger(WFSubscriber.class);
+  private static final Logger logger = Logger.getLogger(WFSubscriber.class);
 
-	private WFGauge myGauge;
+  private WFGauge myGauge;
 	
-	public WFSubscriber(WFGauge wfg) 
-		throws SienaException, IOException {
-		myGauge = wfg;	
-		setup();
-	}
+  public WFSubscriber(WFGauge wfg) 
+    throws SienaException, IOException {
 
-	public void notify(Notification e) {
-		ClientDesc currentClient;
+    myGauge = wfg;	
+    setup();
+  }
 
-        logger.debug("received " + e);
+  public void notify(Notification e) {
+    ClientDesc currentClient;
 
-    	String id = String.valueOf(e.getAttribute(SienaConstants.CLIENT_ID).longValue());
-    	Hashtable ht = myGauge.getGroupClients();	
-    	currentClient = (ClientDesc)ht.get(id);
-    	if (currentClient == null) {
-    		ht.put(id, currentClient = new ClientDesc(id));
-    		myGauge.getBucket().update(id, currentClient);	
-    	}
-    	
-    	/*if (e.getAttribute("Start") != null) {
-    		//new client up, update the gauge  with its info
-    		System.out.println("Connecting Client - " + id);
-    		long st = e.getAttribute("Start").longValue();
-    		// if this is the first client issuing a "Start", start the gauge thread*/
-    		if (! myGauge.isRunning()) {
-    			// nominal start time = 1st client start time
-    			long st = System.currentTimeMillis();
-                myGauge.setStartTime(st);
-    			currentClient.setStartTime(st);
-	    		logger.debug("starting nominal");
-                myGauge.startNominal();
-	    	}     		
-		/*	else {
-				// normalize all the client start times
-				currentClient.setStartTime(myGauge.getStartTime());
-			}
-    	}
-    	else */{
-    		//normalize download time
-		  long t = e.getAttribute(SienaConstants.PROBE_TIME).longValue();// - myGauge.getStartTime();
+    logger.debug("received " + e);
 
-		  currentClient.setFrame(e.getAttribute(SienaConstants.LEFTBOUND).intValue(),
-					 e.getAttribute(SienaConstants.MOMENT).intValue(),
-					 e.getAttribute(SienaConstants.RIGHTBOUND).intValue(),
-					 t,
-					 e.getAttribute(SienaConstants.LEVEL).intValue(),
-					 e.getAttribute(SienaConstants.SIZE).intValue());
-		}
-	}
+    String id = String.valueOf(e.getAttribute(SienaConstants.CLIENT_ID).longValue());
+    Hashtable ht = myGauge.getGroupClients();	
+    currentClient = (ClientDesc)ht.get(id);
+    if (currentClient == null) {
+      ht.put(id, currentClient = new ClientDesc(id));
+      myGauge.getBucket().update(id, currentClient);
+    }
+    
+    
+    if (e.getAttribute(SienaConstants.AI2TV_FRAME) != null){
+      
+      //normalize download time
+      long t = myGauge.currentTime(); // e.getAttribute(SienaConstants.PROBE_TIME).longValue();// - myGauge.getStartTime();
+      currentClient.setFrame(e.getAttribute(SienaConstants.LEFTBOUND).intValue(),
+			     e.getAttribute(SienaConstants.MOMENT).intValue(),
+			     e.getAttribute(SienaConstants.RIGHTBOUND).intValue(),
+			     t,
+			     e.getAttribute(SienaConstants.LEVEL).intValue(),
+			     e.getAttribute(SienaConstants.SIZE).intValue());
+      
+    } else {
+      String action = e.getAttribute(SienaConstants.AI2TV_VIDEO_ACTION).stringValue();
+      if (action.equals(SienaConstants.PLAY)) {
+	//check if clients are already running
+	if (! myGauge.isRunning()) {
+	  // nominal start time = 1st client start time
+	  long st = myGauge.currentTime(); // System.currentTimeMillis();
+	  myGauge.setStartTime(st);
+	  currentClient.setStartTime(st);
+	  logger.debug("starting nominal");
+	  myGauge.startNominal();
+	} 
+	myGauge.startTime();
 	
+      } else if (action.equals(SienaConstants.PAUSE)) {
+	  myGauge.pauseTime();
+
+      } else if (action.equals(SienaConstants.STOP)) {
+	myGauge.stopTime();
+	
+      } else if (action.equals(SienaConstants.GOTO)) {
+	myGauge.gotoTime(e.getAttribute(SienaConstants.NEWTIME).intValue());
+      
+      } else {
+	// should throw an unknown error, or something
+	logger.debug("Received unknown event: " + e);
+      }
+    }
+  }
 }
