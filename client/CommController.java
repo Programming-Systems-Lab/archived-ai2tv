0ea120c2300a4d37204f3efda12945c1671f15c6
dp2041 <dp2041>
2003-08-29 05:42:39 +0000

    changed from using a ThinClient to using a HierarchicalDispatcher

--- client/CommController.java
+++ client/CommController.java
@@ -1,17 +1,17 @@
 /*
- * @(#)CommController.java
- *
- * Copyright (c) 2001: The Trustees of Columbia University in the City of New York.  All Rights Reserved
- *
- * Copyright (c) 2001: @author Dan Phung
- * Last modified by: Dan Phung (dp2041@cs.columbia.edu)
- *
- * CVS version control block - do not edit manually
- *  $RCSfile$
- *  $Revision$
- *  $Date$
- *  $Source$
- */
+* @(#)CommController.java
+*
+* Copyright (c) 2001: The Trustees of Columbia University in the City of New York.  All Rights Reserved
+*
+* Copyright (c) 2001: @author Dan Phung
+* Last modified by: Dan Phung (dp2041@cs.columbia.edu)
+*
+* CVS version control block - do not edit manually
+*  $RCSfile$
+*  $Revision$
+*  $Date$
+*  $Source$
+*/
 
 package psl.ai2tv.client;
 
@@ -20,13 +20,15 @@ import java.util.Calendar;
 import java.net.*;
 import psl.ai2tv.SienaConstants;
 import siena.*;
+import siena.comm.InvalidSenderException;
+import siena.comm.PacketSenderException;
 
 /**
  * The Communications Controller of the AI2TV client.  Main layer
  * of communication handling is done through Siena events.
  *
  * WF related probes:
- * 1)  
+ * 1)
  *
  *
  *
@@ -37,7 +39,8 @@ class CommController implements Notifiable{
   public static final int DEBUG = 0;
 
   private Client _client;
-  private ThinClient _siena;
+  // private ThinClient _siena;
+  private HierarchicalDispatcher _siena;
   private String _sienaServer;
   private boolean _isActive = false;
 
@@ -93,7 +96,7 @@ class CommController implements Notifiable{
       Filter filter = new Filter();
       filter.addConstraint(SienaConstants.GET_ACTIVE_VSIDS_REPLY, Op.ANY, "FOO");
       _siena.subscribe(filter, this);
-      
+
       filter = new Filter();
       filter.addConstraint(SienaConstants.JOIN_NEW_VSID_REPLY, Op.ANY, "FOO");
       _siena.subscribe(filter, this);
@@ -106,23 +109,23 @@ class CommController implements Notifiable{
    * setup the communications with the Video related actions
    */
   private void setupSienaListener(){
+    _siena = new HierarchicalDispatcher();
+
     try {
-      _siena = new ThinClient(_sienaServer);
-
-      // subsribe to the events (specified in the method)
-      //setupFilter();
-      // Client.out.println("subscribing for " + filter.toString());
-      _isActive = true;
-
-    } catch (SienaException e) {
-      // ; // WTF?
-      // } catch (siena.comm.InvalidSenderException e) {
-      System.err.println ("Cannot connect to Siena bus: "  + e);
-      // _siena = null;
-      e.printStackTrace(System.err);
+      _siena.setMaster(_sienaServer);
+    } catch (InvalidSenderException e) {
+      System.err.println("Caught Exception " + e);
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.err.println("Caught Exception " + e);
+      e.printStackTrace();
+    } catch (PacketSenderException e) {
+      System.err.println("Caught Exception " + e);
+      e.printStackTrace();
     }
+    _isActive = true;
   }
-  
+
   /**
    * get the active videos from the WG server
    */
@@ -156,36 +159,32 @@ class CommController implements Notifiable{
    * showdown the communications layer
    */
   void shutdown(){
-     Client.debug.println("Shutting down CommController");
+    Client.debug.println("Shutting down CommController");
     Client.debug.println("Unsubscribing to Siena server");
-    try {
-      Notification shutdownEvent = new Notification();
-      shutdownEvent.putAttribute(SienaConstants.REMOVE_USER_FROM_VSID, "FOO");
-      publishNotification(shutdownEvent);
+    Notification shutdownEvent = new Notification();
+    shutdownEvent.putAttribute(SienaConstants.REMOVE_USER_FROM_VSID, "FOO");
+    publishNotification(shutdownEvent);
 
-      shutdownEvent = new Notification();
-      shutdownEvent.putAttribute(SienaConstants.AI2TV_CLIENT_SHUTDOWN, "");
-      publishNotification(shutdownEvent);
+    shutdownEvent = new Notification();
+    shutdownEvent.putAttribute(SienaConstants.AI2TV_CLIENT_SHUTDOWN, "");
+    publishNotification(shutdownEvent);
 
-      Filter filter = new Filter();
-      filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, Op.ANY, "FOO");
-      _siena.unsubscribe(filter, this);
+    Filter filter = new Filter();
+    filter.addConstraint(SienaConstants.AI2TV_VIDEO_ACTION, Op.ANY, "FOO");
+    _siena.unsubscribe(filter, this);
 
-      filter = new Filter();
-      filter.addConstraint(SienaConstants.GET_ACTIVE_VSIDS_REPLY, Op.ANY, "FOO");
-      _siena.unsubscribe(filter, this);
+    filter = new Filter();
+    filter.addConstraint(SienaConstants.GET_ACTIVE_VSIDS_REPLY, Op.ANY, "FOO");
+    _siena.unsubscribe(filter, this);
 
-      filter = new Filter();
-      filter.addConstraint(SienaConstants.JOIN_NEW_VSID_REPLY, Op.ANY, "FOO");
-      _siena.unsubscribe(filter, this);
-    } catch (siena.SienaException e) {
-      Client.err.println("error:" + e);
-    }
+    filter = new Filter();
+    filter.addConstraint(SienaConstants.JOIN_NEW_VSID_REPLY, Op.ANY, "FOO");
+    _siena.unsubscribe(filter, this);
     Client.debug.println("Shutting down Siena server");
     _siena.shutdown();
     _isActive = false;
   }
-  
+
   /**
    * recieve a notification from the Siena server
    *
@@ -194,15 +193,15 @@ class CommController implements Notifiable{
   public void notify(Notification event) {
     handleNotification(event);
   };
-  
+
   /**
    * recieve some notifications from the Siena server
    *
    * @param events: Notifications sent from the Siena server
    */
-  public void notify(Notification [] events) { 
+  public void notify(Notification [] events) {
     for (int i=0; i<events.length; i++)
-      handleNotification(events[i]);      
+      handleNotification(events[i]);
   }
 
   /**
@@ -211,11 +210,11 @@ class CommController implements Notifiable{
    * @param event: Notification sent by the Siena server
    */
   private void handleNotification(Notification event){
-    
-    // Client.out.println("handleNotification(): I just got this event:" + event + ": at : " 
+
+    // Client.out.println("handleNotification(): I just got this event:" + event + ": at : "
     // + Calendar.getInstance().getTime());
-    Client.debug.println("handleNotification(): I just got this event:" + event + ": at : " 
-		       + Calendar.getInstance().getTime());
+    Client.debug.println("handleNotification(): I just got this event:" + event + ": at : "
+        + Calendar.getInstance().getTime());
 
     AttributeValue absAttrib = event.getAttribute(SienaConstants.ABS_TIME_SENT);
     long absTimeSent = -1;
@@ -225,39 +224,39 @@ class CommController implements Notifiable{
 
     if (event.getAttribute(SienaConstants.AI2TV_VIDEO_ACTION) != null){
       String attrib = event.getAttribute(SienaConstants.AI2TV_VIDEO_ACTION).stringValue();
-      
+
       if (attrib.equals(SienaConstants.PLAY)){
-	Client.debug.println("CommController: PLAY action event received");
-	_client.commPlay(absTimeSent); 
+        Client.debug.println("CommController: PLAY action event received");
+        _client.commPlay(absTimeSent);
       } else if (attrib.equals(SienaConstants.STOP)){
-	Client.debug.println("CommController: PLAY action event received");
-	_client.commStop();
+        Client.debug.println("CommController: PLAY action event received");
+        _client.commStop();
 
       } else if (attrib.equals(SienaConstants.PAUSE)){
-	Client.debug.println("CommController: PLAY action event received");
-	_client.commPause(absTimeSent);
+        Client.debug.println("CommController: PLAY action event received");
+        _client.commPause(absTimeSent);
 
       } else if (attrib.equals(SienaConstants.GOTO)){
-	Client.debug.println("CommController: PLAY action event received");
-	_client.commGoto(absTimeSent, event.getAttribute(SienaConstants.NEWTIME).intValue());
+        Client.debug.println("CommController: PLAY action event received");
+        _client.commGoto(absTimeSent, event.getAttribute(SienaConstants.NEWTIME).intValue());
       } else {
-	Client.err.println("AI2TV_VIDEO_ACTION: Notification Error, received unknown attribute: " + attrib);
+        Client.err.println("AI2TV_VIDEO_ACTION: Notification Error, received unknown attribute: " + attrib);
       }
-      
+
     } else if (event.getAttribute(SienaConstants.GET_ACTIVE_VSIDS_REPLY) != null){
       if (event.getAttribute(SienaConstants.ACTIVE_VSIDS) != null){
-	String activeVSIDs = event.getAttribute(SienaConstants.ACTIVE_VSIDS).stringValue();
-	String activeVSIDInfo = event.getAttribute(SienaConstants.ACTIVE_VSIDS_INFO).stringValue();
-	_client.setActiveVSID(activeVSIDs, activeVSIDInfo);
+        String activeVSIDs = event.getAttribute(SienaConstants.ACTIVE_VSIDS).stringValue();
+        String activeVSIDInfo = event.getAttribute(SienaConstants.ACTIVE_VSIDS_INFO).stringValue();
+        _client.setActiveVSID(activeVSIDs, activeVSIDInfo);
       } else {
-	_client.setActiveVSID(null, null);
+        _client.setActiveVSID(null, null);
       }
 
     } else if (event.getAttribute(SienaConstants.JOIN_NEW_VSID_REPLY) != null){
       String vsid = event.getAttribute(SienaConstants.JOIN_NEW_VSID_REPLY).stringValue();
       _client.setVSID(vsid);
       _client.setWaitForWGReply(false);
-      
+
     } else {
       Client.err.println("Notification Error, received unknown event");
     }
@@ -308,7 +307,7 @@ class CommController implements Notifiable{
     Client.out.println("CommController publishing event: " + event);
     publishNotification(event);
   }
-   
+
   /**
    * handle the actual publishing to the Siena server
    *
@@ -321,15 +320,15 @@ class CommController implements Notifiable{
       event.putAttribute(SienaConstants.UID, _client.getUID());
       event.putAttribute(SienaConstants.GID, _client.getGID());
       if (_client.getVSID() != null)
-	event.putAttribute(SienaConstants.VSID, _client.getVSID());
+        event.putAttribute(SienaConstants.VSID, _client.getVSID());
       _siena.publish(event);
     } catch (siena.SienaException e){
       Client.err.println("CommController publishing sienaException: " + e);
-    }  
+    }
   }
 
   // ------- END Viewer initiated actions -------- //
-  
+
 
   // void setTarget(CacheController cc) { cache = cc; }
   boolean isActive() { return _isActive; }
